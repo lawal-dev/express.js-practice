c5966a06a0fc916fa3a51d30066cae0c148608a2
lawal-dev <optimextech@gmail.com>
2026-07-13 12:51:59 +0100

    get a single user implemented

--- src/server.js
+++ src/server.js
@@ -1,14 +1,42 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const PORT = process.env.PORT || 4000
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const users = [
+  {
+    id: 1,
+    username: "toheeb",
+    course: "CSC",
+  },
+  { 
+    id: 2,
+    username: 'sarah',
+    course: 'IT'
+
+  },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Welcome here", users });
+});
+
+app.get("/users/:id", (req, res)=>{
+    const id = req.params.id
+    const userId = parseInt(id)
+    const user = users.find((user)=>{
+       return user.id === userId
+    })
+    if(user){
+        res.status(200).send(user)
+    }
+    else
+    {
+        res.status(404).send({msg: "user not found"})
+    }
 
 
-
-app.get("/", (req, res)=>{
-    res.status(200).send({msg: "Welcome here", name: "Toheeb Lawal"})
 })
-
-app.listen(PORT, ()=>{
-    console.log("Listening on PORT 4000")
-})
+app.listen(PORT, () => {
+  console.log("Listening on PORT 4000");
+});
