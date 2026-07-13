5432c3612f61fc524fe6e3996abb880c3cbfada1
lawal-dev <optimextech@gmail.com>
2026-07-13 15:04:20 +0100

    query done

--- src/server.js
+++ src/server.js
@@ -17,8 +17,22 @@ const users = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.status(200).send({ msg: "Welcome here", users });
+app.get("/users", (req, res) => {
+    console.log(req.query)
+    const {search} = req.query
+    console.log(search)
+    if(!search){
+        res.status(200).send(users)
+    }
+     else{
+        res.status(200).send(
+            users.filter((user)=>{
+                return user.username.includes(search)
+            })
+        )
+    }
+   
+//   res.status(200).send({ msg: "Welcome here", users });
 });
 
 app.get("/users/:id", (req, res)=>{
